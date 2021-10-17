0943ad917f4f5c73a76490d0157c556707ec84ea
Tristan <73963573+tsj845@users.noreply.github.com>
2021-10-17 10:50:12 -0400

    Update gk.js

--- gk.js
+++ gk.js
@@ -3,11 +3,23 @@ class GKBoard {
 		this.board = document.getElementById("gk-board");
 		this.board.hidden = false;
 		this.map = {};
+		this.localCopy = [];
 		this.size = size;
+		this.cx = 0;
+		this.cy = 0;
+		this.helptext = {};
 		this.makeboard(size);
 	}
-	render () {
-		//
+	render (chx, chy) {
+		chx = (chx==undefined?this.cx:chx);
+		chy = (chy==undefined?this.cy:chy);
+		this.cx = chx;
+		this.cy = chy;
+		for (let y = 0; y < this.size; y ++) {
+			for (let x = 0; x < this.size; x ++) {
+				this.set_src(x, y, this.map[this.localCopy[this.cy*this.size+y][this.cx*this.size+x]]);
+			}
+		}
 	}
 	set_src (x, y, v) {
 		this.board.children[y*this.size+x].src = v;
@@ -29,3 +41,13 @@ class GKBoard {
 }
 
 const gk = new GKBoard(5);
+
+gk.map[0] = "sprites/test.jpg";
+gk.map[1] = "defaults/empty.png";
+gk.localCopy = [[0,0,0,0,0],
+				[0,1,1,1,0],
+				[0,1,1,1,0],
+				[0,1,1,1,0],
+				[0,0,0,0,0]];
+
+gk.render();
